7b6df016deea09fdb2ab74eaf7768ed292dec74b
fpr <frankpiercereynolds95@gmail.com>
2022-09-01 20:51:26 -0400

    updated server.js with some new functions

--- server.js
+++ server.js
@@ -123,3 +123,136 @@ showDepartments = () => {
     promptUser();
   });
 };
+
+// show roles
+showRoles = () => {
+    console.log('Showing roles\n');
+    const sql = `SELECT role.id, role.title, department.name AS department
+                 FROM role
+                 INNER JOIN department ON role.department_id = department.id`;   
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
+    })
+  };
+
+// show employees
+showEmployees = () => {
+    console.log('Showing employees\n'); 
+  const sql = `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      role.title, 
+                      department.name AS department,
+                      role.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+               FROM employee
+                      LEFT JOIN role ON employee.role_id = role.id
+                      LEFT JOIN department ON role.department_id = department.id
+                      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// add a dept
+addDepartment = () => {
+    inquirer.prompt([
+      {
+        type: 'input', 
+        name: 'addDept',
+        message: 'What department do you want to add?',
+        validate: addDept => {
+          if (addDept) {
+              return true;
+          } else {
+              console.log('Please enter a department');
+              return false;
+          }
+        }
+      }
+    ])
+      .then(answer => {
+        const sql = `INSERT INTO department (name)
+                    VALUES (?)`;
+        connection.query(sql, answer.addDept, (err, result) => {
+          if (err) throw err;
+          console.log("Added " + answer.addDept + " to departments"); 
+  
+          showDepartments();
+      });
+    });
+  };
+
+// add role
+addRole = () => {
+    inquirer.prompt([
+      {
+        type: 'input', 
+        name: 'role',
+        message: "Enter name of role to add.",
+        validate: addRole => {
+          if (addRole) {
+              return true;
+          } else {
+              console.log('Please enter a role');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input', 
+        name: 'salary',
+        message: "Enter role salary.",
+        validate: addSalary => {
+          if (isNAN(addSalary)) {
+              return true;
+          } else {
+              console.log('Please enter a salary');
+              return false;
+          }
+        }
+      }
+    ])
+      .then(answer => {
+        const params = [answer.role, answer.salary];
+  
+        // select dept from dept table
+        const roleSql = `SELECT name, id FROM department`; 
+  
+        connection.promise().query(roleSql, (err, data) => {
+          if (err) throw err; 
+      
+          const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+  
+          inquirer.prompt([
+          {
+            type: 'list', 
+            name: 'dept',
+            message: 'Which department is this role in?',
+            choices: dept
+          }
+          ])
+            .then(deptChoice => {
+              const dept = deptChoice.dept;
+              params.push(dept);
+  
+              const sql = `INSERT INTO role (title, salary, department_id)
+                          VALUES (?, ?, ?)`;
+  
+              connection.query(sql, params, (err, result) => {
+                if (err) throw err;
+                console.log('Added' + answer.role + ' to roles'); 
+  
+                showRoles();
+         });
+       });
+     });
+   });
+  };
+  
+// func to add employee
